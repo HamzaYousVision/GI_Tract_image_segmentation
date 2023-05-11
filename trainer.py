ae90cb23aae2b55826b1e08a103721e88b6df10d
Hamza Yous <hyous@usthb.dz>
2023-05-11 08:54:38 +0100

    feat: added trainer

---
+++ trainer.py
@@ -0,0 +1,10 @@
+class Trainer: 
+    def __init__(self): 
+        self.epochs = 10 
+        self.define_optimizer()
+    
+    def define_optimizer(self):
+        pass 
+
+    def train(self): 
+        pass
